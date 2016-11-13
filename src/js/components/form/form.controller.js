0e290684b3e3588856074d8ab4fdd9583f2b1552
Ryan "archaen" Pando <vretha@gmail.com>
2016-11-13 15:44:58 -0700

    Form now takes values from edit.

--- src/js/components/form/form.controller.js
+++ src/js/components/form/form.controller.js
@@ -6,14 +6,29 @@
     .module('myApp.components.form', [])
     .controller('formController', formController);
 
-  formController.$inject = ['$scope', '$location', 'ajax'];
-  function formController($scope, $location, ajax) {
+  formController.$inject = ['$scope', '$location', 'ajax', 'donut'];
+  function formController($scope, $location, ajax, donut) {
     /*jshint validthis: true */
     const vm = this;
-    vm.id = null;
-    vm.name = null;
-    vm.topping = null;
-    vm.price = null;
+    vm.id = donut.id;
+    vm.name = donut.name;
+    vm.topping = donut.topping;
+    vm.price = donut.price;
+
+    this.submit = () => {
+      const user = {
+        name: this.username,
+        password: this.password,
+        email: this.email,
+        zip: this.zip
+      };
+      this.users.push(user);
+      this.username = null;
+      this.email = null;
+      this.password = null;
+      this.zip = null;
+      $scope.formie.$setPristine();
+    };
   }
 
 })();
